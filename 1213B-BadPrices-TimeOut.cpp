d804daf256a198f485f6f6e1b05112fe4b4e44e7
mlk <alvesk.mak@gmail.com>
2022-05-23 02:08:53 -0300

    [1213B] Solution with timeout :/

--- 1213B-BadPrices-TimeOut.cpp
+++ 1213B-BadPrices-TimeOut.cpp
@@ -1,47 +1,30 @@
-    #include<iostream>
-    #include<list>
-    using namespace std;
-     
-     
-    int fun(list<int>*, int);
-    int main ()
-    {
-    	int k, m;
-    	cin>>k;
-    	while(k>0)
-    	{
-    		cin>>m;
-    		list<int> * ptr;
-    		ptr = new list<int>[m];
-    		for(int i = 0; i <m; i++){
-    			int val;
-    			cin>>val;
-    			ptr->push_front(val);
-    		}
-    		cout<<fun(ptr, m)<<endl;
-    		delete [] ptr;
-    		k--;
-    	}
-    }
-     
-    int fun(list<int>* ptr,int  m){
-    	list<int>::iterator it, itr;
-    	int days = 0;
-    	it = ptr->begin();
-    	itr = ptr->begin();
-    	do{
-    		for(int i  = 0; i<ptr->size(); i++){
-    			advance(itr, i);
-    			if(*itr>*it){
-    				days++;
-    				ptr->erase(itr);
-    				i--;
-    				}
-    				itr = ptr->begin();
-    			}
-    		it++;
-    		ptr->pop_front();
-    		itr = ptr->begin();
-    	}while(it != ptr->end());
-    	return days;
-    }
+#include<iostream>
+using namespace std;
+int main ()
+				{
+				int k, m;
+				cin>>k;
+				while(k>0)
+								{
+								cin>>m;
+								int * ptr;
+								ptr = new int [m];
+								for(int i = 0; i <m; i++){
+									int val;
+									cin>>ptr[i];
+								}
+								int it = 0, days = 0;
+								do{
+								for(int i  = it + 1; i<m; i++){
+										if(ptr[i]<ptr[it]){
+											days++;
+											break;
+										}
+									}
+								it++;
+								}while(it < m);
+								cout<<days<<endl;
+								delete [] ptr;
+								k--;
+								}
+				}
