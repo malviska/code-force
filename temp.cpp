469056b88b5cecfc9f0cae3b7a0c690adb0f0d46
mlk <alvesk.mak@gmail.com>
2022-05-30 04:09:25 -0300

    beecrowd2653

--- temp.cpp
+++ temp.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define _ ios_base::sync_with_stdio(0);cin.tie(0)
+#define _ iosbase::sync_with_stdio(0);cin.tie(0)
 #define endl '\n'
 #define f first
 #define s second
@@ -11,13 +11,7 @@ const int INF = 0x3f3f3f3f;
 const ll LINF =  0x3f3f3f3f3f3f3f3fll;
 
 int main()
-{ _;
-
-	string joia;
-	set<string> joias;
-	while(cin>> joia){
-		joias.insert(joia);
-	}
-	cout<<joias.size()<<endl;
+{
+	
 	return 0;
 }
